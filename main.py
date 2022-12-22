f5cc724b5a857cf2980bf26ae1aad4c00f813707
birmanov <jambul@birmanov.com>
2022-12-22 15:33:48 +0600

    1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-c = 105
+target = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target_string = "Python: Hello, world"
 
-print(bin(c))
-print(hex(c))
-print(oct(c))
-print(type(b))
+int = 0
+
+while target:
+    print(target.pop(0))
