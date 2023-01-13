afeb7ca1969dbf69d50257bfb6a7de59f60cc1ee
birmanov <jambul@birmanov.com>
2023-01-13 13:15:17 +0600

    new

--- new course.py
+++ new course.py
@@ -2,3 +2,8 @@ print("Привет!!!")
 print("Я учусь программировать на Python!!!")
 print('', 'P', 'Y', 'T', 'H', 'O', 'N', end='*** \n')
 print("***Version Python 3.10***")
+
+print("Привет", end="!!! \n")
+print("Я учусь программировать на Python", end="!!! \n")
+print('','P','Y','T','H','O','N', sep='***', end='*** \n')
+print('***Version Python 3.10', end='***')
