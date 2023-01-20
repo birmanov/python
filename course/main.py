8b5122fd4a5b4ed38e93126d87b0809911fdcdde
Birmanov <jambul@birmanov.com>
2023-01-20 15:44:58 +0600

    123

--- course/main.py
+++ course/main.py
@@ -1,4 +1,9 @@
-name = [1, 5]
+my_commet = "text Text text"
 
+print(len(my_commet))
 
-print(name)
+print(my_commet.replace('Text', 'line'))
+
+print(my_commet.count('t'))
+
+print(my_commet[1:13])
