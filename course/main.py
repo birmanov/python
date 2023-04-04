3286084a5152b59c64c4718617cc2535d11aaed6
Birmanov <jambul@birmanov.com>
2023-04-04 10:24:49 +0600

    ++

--- course/main.py
+++ course/main.py
@@ -3,4 +3,14 @@ my_frunt = ['apple', 'banana', 'lime']
 my_cunt = ['125', '456', '1645']
 my_gfsg = [True, 'hi!', '222,565']
 
-print(my_frunt[-1])
+print(my_frunt)
+
+print(my_cunt)
+
+my_cunt[2] = 999
+
+print(my_cunt)
+
+del my_gfsg[2]
+
+print(my_gfsg)
