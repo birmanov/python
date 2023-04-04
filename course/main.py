5378762886db3793e9a3126cb85100aad1ab0632
Birmanov <jambul@birmanov.com>
2023-04-04 09:40:21 +0600

    ++

--- course/main.py
+++ course/main.py
@@ -1,2 +1,6 @@
-print(bool)
-print(dir(bool))
+user_inputs = [True, 'Hi!', ':-)', 10.5]
+my_frunt = ['apple', 'banana', 'lime']
+my_cunt = ['125', '456', '1645']
+my_gfsg = [True, 'hi!', '222,565']
+
+print(my_frunt[-1])
