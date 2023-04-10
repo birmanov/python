77bc2d918fb0c0da70356e0a6ce1310a63aaf028
Birmanov <jambul@birmanov.com>
2023-04-10 10:22:44 +0600

    ++

---
+++ course/task1.py
@@ -0,0 +1,27 @@
+# 1
+my_list = ['25', 'privet', '!@#$', '253,254', True]
+print(my_list)
+
+# 2
+my_list.pop(2)
+print(my_list)
+
+# 3
+print(len(my_list))
+
+# 4
+my_list.reverse()
+print(my_list)
+
+# 5
+my_list2 = my_list.copy()
+my_list2.append(35)
+my_list2.append(9999)
+print(my_list2)
+
+# 6
+my_list.extend(my_list2)
+print(my_list)
+
+# 7
+print(my_list[:6])
